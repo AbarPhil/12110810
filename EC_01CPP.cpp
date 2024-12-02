dbf18c2a4ce98bd6d88c025ce16eebc710e51adb
AbarPhil <abarphil@gmail.com>
2024-12-02 16:50:00 +0000

    Update EC_01CPP.cpp

--- EC_01CPP.cpp
+++ EC_01CPP.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 
 using namespace std;
@@ -7,5 +8,19 @@ int main() {
     cout << "Introduza um número: ";
     cin >> number;
     cout << "O número introduzido foi: " << number << endl;
+    return 0;
+}    */
+
+#include<iostream>
+using namespace std;
+int main()  {
+    int idade;
+    float altura;
+    cout << "Introduz a tua idade: ";
+        cin >> idade;
+    cout << "Introduz a tua altura: ";
+        cin >> altura;
+        
+    cout << "A tua idade é " << idade << " e a tua altura é " << altura
     return 0;
 }
